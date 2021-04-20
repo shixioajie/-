ef0383b83f46c007fab6f7ab5aa0b1c896879ac3
shixioajie <2324966855@qq.com>
2021-04-20 22:07:15 +0800

    webpack 环境配置+出入输出基本使用

---
+++ 1-webpack/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+// console.log(path.resolve());
+// console.log(path.join(__dirname,'dist'));
+
+const paths = path.join(__dirname,'./dist').split('\\').join('/');
+console.log(paths)
+
+const config = {
+    entry:"./src/index.js",
+    output:{
+        filename: "bundle2.js",
+        path:paths
+    }
+}
+
+module.exports = config;
+
--- README.md
+++ README.md
@@ -5,7 +5,8 @@
 #### webpack 
 [使用默认选项配置] npm init -y
 [局部安装] npm install webpack webpack-cli --save-dev (安装在开发模式下) 或 yarn add webpack webpack-cli -D
-[查看版本]可以指令 `npx webpack --version`
+[查看版本]可以指令 `npx webpack --version` (webpack5 以后可以使用)
 [全局安装]npm install webpack -g 或者 yarn global add webpack webpack-cli
+[调用] npm run package.json scripts 等于的内容
 
 
